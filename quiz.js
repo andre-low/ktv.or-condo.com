6c11c6600ef8348d83ac291aac9e716bc52010dc
Andre Low <andre-low@users.noreply.github.com>
2021-07-18 23:46:30 +0800

    Initial commit

---
+++ quiz.js
@@ -0,0 +1,339 @@
+/*
+  Quick quiz bootstrap extension
+*/
+
+(function ($) {
+  // keep track of number of quizes added to page
+  var quiz_count = 0;
+
+  // add jQuery selection method to create
+  // quiz structure from question json file
+  // "filename" can be path to question json
+  // or javascript object
+  $.fn.quiz = function (filename) {
+    if (typeof filename === 'string') {
+      $.getJSON(filename, render.bind(this));
+    } else {
+      render.call(this, filename);
+    }
+  };
+
+  // create html structure for quiz
+  // using loaded questions json
+  function render(quiz_opts) {
+    // list of questions to insert into quiz
+    var questions = quiz_opts.questions;
+
+    // keep track of the state of correct
+    // answers to the quiz so far
+    var state = {
+      correct: 0,
+      total: questions.length,
+    };
+
+    var $quiz = $(this)
+      .attr('class', 'carousel slide')
+      .attr('data-ride', 'carousel');
+
+    // unique ID for container to refer to in carousel
+    var name = $quiz.attr('id') || 'urban_quiz_' + ++quiz_count;
+
+    $quiz.attr('id', name);
+
+    var height = $quiz.height();
+
+    /*
+    Add carousel indicators
+  */
+
+    /*
+    Slides container div
+  */
+    var $slides = $('<div>')
+      .attr('class', 'carousel-inner')
+      .attr('role', 'listbox')
+      .appendTo($quiz);
+
+    /*
+    Create title slide
+  */
+    var $title_slide = $('<div>')
+      .attr('class', 'item active')
+      .attr('height', height + 'px')
+      .appendTo($slides);
+
+    $('<h1>')
+      .text(quiz_opts.title)
+      .attr('class', 'quiz-title')
+      .appendTo($title_slide);
+
+    var $start_button = $('<div>')
+      .attr('class', 'quiz-answers')
+      .appendTo($title_slide);
+
+    var $indicators = $('<ol>').attr('class', 'progress-circles');
+
+    $('<button>')
+      .attr('class', 'quiz-button btn')
+      .text('Take the quiz!')
+      .click(function () {
+        $quiz.carousel('next');
+        $indicators.addClass('show');
+
+        $('.active .quiz-button.btn').each(function () {
+          console.log(this.getBoundingClientRect());
+          $(this).css('margin-left', function () {
+            return (250 - this.getBoundingClientRect().width) * 0.5 + 'px';
+          });
+        });
+      })
+      .appendTo($start_button);
+
+    $indicators.appendTo($quiz);
+
+    $.each(questions, function (question_index, question) {
+      $('<li>')
+        .attr('class', question_index ? '' : 'dark')
+        .appendTo($indicators);
+    });
+
+    /*
+    Add all question slides
+  */
+    $.each(questions, function (question_index, question) {
+      var last_question = question_index + 1 === state.total;
+
+      var $item = $('<div>')
+        .attr('class', 'item')
+        .attr('height', height + 'px')
+        .appendTo($slides);
+      var $img_div;
+      if (question.image) {
+        $img_div = $('<div>').attr('class', 'question-image').appendTo($item);
+        $('<img>')
+          .attr('class', 'img-responsive')
+          .attr('src', question.image)
+          .appendTo($img_div);
+        $('<p>')
+          .text(question.image_credit)
+          .attr('class', 'image-credit')
+          .appendTo($img_div);
+      }
+      $('<div>')
+        .attr('class', 'quiz-question')
+        .html(question.prompt)
+        .appendTo($item);
+
+      var $answers = $('<div>').attr('class', 'quiz-answers').appendTo($item);
+
+      // if the question has an image
+      // append a container with the image to the item
+
+      // for each possible answer to the question
+      // add a button with a click event
+      $.each(question.answers, function (answer_index, answer) {
+        // create an answer button div
+        // and add to the answer container
+        var ans_btn = $('<div>')
+          .attr('class', 'quiz-button btn')
+          .html(answer)
+          .appendTo($answers);
+
+        // This question is correct if it's
+        // index is the correct index
+        var correct = question.correct.index === answer_index;
+
+        // default opts for both outcomes
+        var opts = {
+          allowOutsideClick: false,
+          allowEscapeKey: false,
+          confirmButtonText: 'Next Question',
+          html: true,
+          confirmButtonColor: '#0096D2',
+        };
+
+        // set options for correct/incorrect
+        // answer dialogue
+        if (correct) {
+          opts = $.extend(opts, {
+            title: 'Sui!',
+            text:
+              'So clever' +
+              (question.correct.image
+                ? '<br><br><img class="correct-image" src="' +
+                  question.correct.image +
+                  '"><br><br>'
+                : '') +
+              (question.correct.text
+                ? '<div class="correct-text">' +
+                  question.correct.text +
+                  '</div>'
+                : ''),
+            type: 'success',
+          });
+        } else {
+          opts = $.extend(opts, {
+            title: 'Alamak!',
+            text:
+              'Wrong lah<br/><br/>' +
+              'The correct answer was "' +
+              question.answers[question.correct.index] +
+              '".' +
+              (question.correct.image
+                ? '<br><br><img class="correct-image" src="' +
+                  question.correct.image +
+                  '"><br><br>'
+                : '') +
+              (question.correct.text
+                ? '<div class="correct-text">' +
+                  question.correct.text +
+                  '</div>'
+                : ''),
+            type: 'error',
+          });
+        }
+
+        if (last_question) {
+          opts.confirmButtonText = 'See your results';
+        }
+
+        // bind click event to answer button,
+        // using specified sweet alert options
+        ans_btn.on('click', function () {
+          function next() {
+            // if correct answer is selected,
+            // keep track in total
+            if (correct) state.correct++;
+            $quiz.carousel('next');
+
+            // if we've reached the final question
+            // set the results text
+            if (last_question) {
+              $results_title.html(resultsText(state));
+              $results_ratio.text(
+                'You got ' +
+                  Math.round(100 * (state.correct / state.total)) +
+                  '% of the questions correct!'
+              );
+              $twitter_link.attr('href', tweet(state, quiz_opts));
+              $facebook_link.attr('href', facebook(state, quiz_opts));
+              $indicators.removeClass('show');
+              // indicate the question number
+              $indicators.find('li').removeClass('dark').eq(0).addClass('dark');
+            } else {
+              // indicate the question number
+              $indicators
+                .find('li')
+                .removeClass('dark')
+                .eq(question_index + 1)
+                .addClass('dark');
+            }
+            // unbind event handler
+            $('.sweet-overlay').off('click', next);
+          }
+
+          // advance to next question on OK click or
+          // click of overlay
+          swal(opts, next);
+          $('.sweet-overlay').on('click', next);
+        });
+      });
+    });
+
+    // final results slide
+    var $results_slide = $('<div>')
+      .attr('class', 'item')
+      .attr('height', height + 'px')
+      .appendTo($slides);
+
+    var $results_title = $('<h1>')
+      .attr('class', 'quiz-title')
+      .appendTo($results_slide);
+
+    var $results_ratio = $('<div>')
+      .attr('class', 'results-ratio')
+      .appendTo($results_slide);
+
+    var $restart_button = $('<div>')
+      .attr('class', 'quiz-answers')
+      .appendTo($results_slide);
+
+    var $social = $('<div>')
+      .attr('class', 'results-social')
+      .html(
+        '<div id = "social-text">Nice right? Share this with your friends and find out how they do!</div>'
+      )
+      .appendTo($results_slide);
+
+    var $twitter_link = $('<a>')
+      .html('<i class="fab fa-twitter"></i>')
+      .appendTo($social);
+
+    var $facebook_link = $('<a>')
+      .html('<i class="fab fa-facebook"></i>')
+      .appendTo($social);
+
+    $('<button>')
+      .attr('class', 'quiz-button btn')
+      .text('Try again?')
+      .click(function () {
+        state.correct = 0;
+        $quiz.carousel(0);
+      })
+      .appendTo($restart_button);
+
+    $quiz.carousel({
+      interval: false,
+    });
+
+    $(window).on('resize', function () {
+      $quiz.find('.item').attr('height', $quiz.height() + 'px');
+    });
+  }
+
+  function resultsText(state) {
+    var ratio = state.correct / state.total;
+    var text;
+
+    switch (true) {
+      case ratio === 1:
+        text =
+          'Amazing! You have a suspiciously firm grasp of lupsup KTVs and terribly named condos. I see bright things in your future.';
+        break;
+      case ratio >= 0.90:
+        text = 'Close but no corona.';
+        break;
+      case ratio >= 0.60:
+        text =
+          'Neither here nor there, the Multi-Ministry Taskforce would be proud!';
+        break;
+      case ratio === 0.50:
+        text = 'Just a pass. Tiger parents everywhere just shed a tear.';
+        break;
+      case ratio < 0.50 && ratio !== 0:
+        text = 'Don\u2019t worry, every school is a good school!';
+        break;
+      case ratio === 0:
+        text =
+          'Amazing! You got none right. That almost takes more effort than doing well!';
+        break;
+    }
+    return text;
+  }
+
+  function tweet(state, opts) {
+    var body =
+      'I scored ' +
+      state.correct +
+      ' out of ' +
+      state.total +
+      " on 'KTV or Condo?'. Can you tell Singapore\u2019s KTVs from our Condos just by the name? Try it here: " +
+      opts.url;
+
+    return 'http://twitter.com/intent/tweet?text=' + encodeURIComponent(body);
+  }
+
+  function facebook(state, opts) {
+    return 'https://www.facebook.com/sharer/sharer.php?u=' + opts.url;
+  }
+})(jQuery);
